3133af5c90e4310bea2f4cc824723c8410628034
Artur Vardanyan <153517516+Artur09011@users.noreply.github.com>
2024-10-22 22:59:59 +0400

    Add files via upload

---
+++ Homework 10.18.24/problem1.py
@@ -0,0 +1,29 @@
+# version1
+
+# number1 = int(input('number1: '))
+# number2 = int(input('number2: '))
+# number3 = int(input('number3: '))
+
+# result = max(number1, number2, number3)
+
+# print(f'the maximum of numbers is {result}')
+
+
+
+# version2
+
+# number1 = int(input('number1: '))
+# number2 = int(input('number2: '))
+# number3 = int(input('number3: '))
+
+# if number1 > number2 and number1 > number3:
+#     print(f'the maximum of numbers is {number1}')
+# elif number2 > number1 and number2 > number3:
+#     print(f'the maximum of numbers is {number2}')
+# elif number3 > number1 and number3 > number2:
+#     print(f'the maximum of numbers is {number3}')
+# else:
+#     print('there are equal numbers please restart')
+
+
+
---
+++ Homework 10.18.24/problem5.py
@@ -0,0 +1,10 @@
+
+
+# numbers = [10, 3, 4, 7, 12, 8, 20, 34, 18]
+# max1 = max(numbers)
+# numbers.remove(max1)
+# max2 = max(numbers)
+
+#print(max2)
+
+
